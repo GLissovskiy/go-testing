8c3ce162e25c21a83dd54936b13af3a0489bf81c
glissovskiy@gmail.com <glissovskiy@gmail.com>
2024-01-04 13:59:21 -0800

    init

---
+++ primeapp/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	n := 3
+
+	fmt.Println("started...")
+
+	_, msg := isPrime(n)
+	fmt.Println(msg)
+
+	fmt.Println("terminated!")
+}
+
+// func returns true if number is not divisible by 2
+func isPrime(n int) (bool, string) {
+	// 0 and 1 are not prime by definition
+
+	if n == 0 || n == 1 {
+		return false, fmt.Sprintf("%d is not prime, by definitions", n)
+	}
+
+	// negative numbers are not prime
+	if n < 0 {
+		return false, "negative numbers are not prime, by definitions."
+	}
+
+	//use the modulus operator repeatedly to see if we have a prime number
+	for i := 2; i <= n/2; i++ {
+		if n%i == 0 {
+			return false, fmt.Sprintf("%d is not a prime number because it is divisible by %d", n, i)
+		}
+	}
+
+	return true, fmt.Sprintf("%d is a prime number!", n)
+
+}
